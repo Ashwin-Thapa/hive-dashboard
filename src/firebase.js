0fba2074bcf7b4c2fcbd58ae1c48207de503122b
Ashwin Thapa <thapaashwin2k@gmail.com>
2025-06-10 14:56:21 +0530

    Read Firebase config from environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { getDatabase } from "firebase/database";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// The config now reads from "process.env" instead of being hard-coded
 const firebaseConfig = {
-  apiKey: "AIzaSyCpWnjKUWqGtNYW2CU7mW-S6z7q1yj8yo0",
-  authDomain: "deploy2-60855.firebaseapp.com",
-  databaseURL: "https://deploy2-60855-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "deploy2-60855",
-  storageBucket: "deploy2-60855.firebasestorage.app",
-  messagingSenderId: "500965196427",
-  appId: "1:500965196427:web:26e2759a172f6b57850a8e",
-  measurementId: "G-PL6RBPF5RX"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Get a reference to the database service
 export const db = getDatabase(app);
