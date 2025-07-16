65f0f5d68f9aba510320a83bb350db16db3e2938
Ashwin Thapa <thapaashwin2k@gmail.com>
2025-07-16 15:20:58 +0530

    Add Raspberry Pi Flask camera server and adjust React dashboard image trigger

--- Nerim/Assignment python/q7.py
+++
@@ -1,7 +0,0 @@
-#Program to iterate and print the elements of a list
-
-L = [1,2,3,4,5,6,7,8,9]
-
-print("The elements in the list are: ")
-for i in L:
-    print(i)
--- Nerim/Assignment python/q8.py
+++
@@ -1,7 +0,0 @@
-#Program to remove odd numbers from a list
-
-L = [1,2,3,4,5,6,7,8,9,10,11,12,14,15,17,19,16,18,20]
-
-odd_num = [i for i in L if i%2 != 0]
-
-print(odd_num)
